8221171ca079799ac749c9f78a205e386ed6eedd
Datenschutzbeauftragter Kanton Zürich <datenschutz@dsb.zh.ch>
2019-07-02 13:54:37 +0200

    New password check version

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: [`
     div {
-      padding: 0px;
-      margin: 0px;
+      height: 100%;
+      padding: 0;
+      margin: 0;
     }
   `]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.cdr.detectChanges();
+  }
+
+  appLogoTextPadding(logo): string {
+    if (logo.clientHeight === 0) {
+      setTimeout(() => {}, 20);
+    }
+    return (logo.clientHeight / 8) + 'px';
+  }
+}
