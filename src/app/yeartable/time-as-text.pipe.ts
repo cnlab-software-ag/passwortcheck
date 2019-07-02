ce8cf5e25d9f95c54c67940913f423af17e11468
Datenschutzbeauftragter Kanton Zürich <datenschutz@dsb.zh.ch>
2019-07-02 14:07:42 +0200

    New password check version

--- src/app/yeartable/time-as-text.pipe.ts
+++ src/app/yeartable/time-as-text.pipe.ts
@@ -1,30 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {Observable} from 'rxjs';
 
 @Pipe({
   name: 'timeAsText'
 })
 export class TimeAsTextPipe implements PipeTransform {
 
-  transform(value: any, args?: any): string {
+  constructor(
+    private translate: TranslateService,
+  ) {}
+
+  transform(value: any, title: string): Observable<string> {
     if (value < 1) {
-      return "Weniger als eine Sekunde";
+      return this.translate.get('timeAsText.lessTenASecond');
     } else if (value < 60) {
-      return this.formattedNumber(value) + " Sekunden";
+      return this.translate.get('timeAsText.seconds', {value: this.formattedNumber(value)});
     } else if (value < 3600) {
-      return this.formattedNumber(value/60) + " Minuten";
+      return this.translate.get('timeAsText.minutes', {value: this.formattedNumber(value / 60)});
     } else if (value < 86400) {
-      return this.formattedNumber(value/3600) + " Stunden";
-    } else if (value < 259200) {
-      return this.formattedNumber(value/86400) + " Tage";
+      return this.translate.get('timeAsText.hours', {value: this.formattedNumber(value / 3600)});
+    } else if (value < 2592000) {
+      return this.translate.get('timeAsText.days', {value: this.formattedNumber(value / 86400)});
     } else if (value < 31536000) {
-      return this.formattedNumber(value/2592000) + " Monate";
+      return this.translate.get('timeAsText.months', {value: this.formattedNumber(value / 2592000)});
     } else if (value < 31536000000000) {
-      return this.formattedNumber(value/31536000) + " Jahre";
+      return this.translate.get('timeAsText.years', {value: this.formattedNumber(value / 31536000)});
     }
-    return "Mehrere Millionen Jahre";
+    return this.translate.get('timeAsText.severalMillionYears');
   }
 
-  formattedNumber(number) {
+  formattedNumber(number: number) {
     return number.toLocaleString('de-CH', {maximumFractionDigits: 0});
   }
 }
--- src/app/yeartable/yeartable.component.ts
+++ src/app/yeartable/yeartable.component.ts
@@ -8,11 +8,15 @@ import {Component, Input} from '@angular/core';
       position: absolute;
       top: 30px;
       background: #bcc4c8;
+      z-index: 999;
       padding: 4px;
       left: 0;
       display:block;
       text-wrap: normal;
       word-wrap: break-word;
+    }
+    .word-break {
+      word-break: break-all;
     }
   `]
 })
